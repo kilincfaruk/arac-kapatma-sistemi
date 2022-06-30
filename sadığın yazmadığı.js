691bde4552e5949bd3348288932663d257e1b6db
kilincfaruk <kilincfaruk2@gmail.com>
2022-06-30 16:11:21 +0300

    changes

---
+++ sadığın yazmadığı.js
@@ -0,0 +1,56 @@
+docunment.getElementById('form-Task').addEventListener('submit', saveTask);
+
+function saveTask(e) {
+
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+
+    let task = {
+        title,
+        description
+    };
+
+    if (localStorage.getItem('tasks') === null) {
+        let = tasks = []
+        tasks.push(tasks);
+        localStorage.setItem('tasks', JSON.stringify(task));
+    }
+    else {
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    getTasks();
+
+    document.getElementById('form-Task').reset();
+    e.preventDefeault();
+}
+
+function deleteTask(title) {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].title == title) {
+            tasks.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    getTasks();
+}
+
+function getTasks() {
+    let tasks = JOSN.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById('tasks');
+    tasksView.innerHTML = '';
+
+    for (let i = 0; i < tasks.length; i++) {
+        let title = task[i].title;
+        let description = tasks[i].description;
+
+        tasksView.innerHTML +=
+            '
+
+        ';
+    }
+}
